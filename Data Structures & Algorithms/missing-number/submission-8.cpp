fdcfb91462896d45918d981182a0d550d6ed1d66
Atharva Ghuge <atharvaghuge16@gmail.com>
2026-08-13 12:39:47 +0530

    Add: missing-number - submission-8

---
+++ Data Structures & Algorithms/missing-number/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(st.count(i)){
+                continue;
+            }
+            else{
+                return i;
+            }
+        }
+        
+    }
+};
